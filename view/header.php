71ac8a5c9bae477e9c1834344be94c48e0c518e8
tcalvk <103088458+tcalvk@users.noreply.github.com>
2023-09-25 22:38:20 -0600

    done

--- view/header.php
+++ view/header.php
@@ -14,17 +14,17 @@
 <body>
     <div class="container">
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="/crm/homepage.php">Corsaire CRM</a>
+            <a class="navbar-brand" href="/homepage.php">Corsaire CRM</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
                     <li class="nav-item active">
-                        <a class="nav-link" href="/crm/homepage.php">Home <span class="sr-only">(current)</span></a>
+                        <a class="nav-link" href="/homepage.php">Home <span class="sr-only">(current)</span></a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="/crm/customer/index.php?action=list_customers">Customers</a>
+                        <a class="nav-link" href="/customer/index.php?action=list_customers">Customers</a>
                     </li>
                     <li class="nav-link disabled">
                         <form action="/crm/index.php" method="post">
