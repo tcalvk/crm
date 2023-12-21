819316428e1758dcca574d528dd00b3fd4670257
tcalvk <103088458+tcalvk@users.noreply.github.com>
2023-12-20 18:21:28 -0700

    initial commit

--- db_changes.sql
+++ db_changes.sql
@@ -233,3 +233,88 @@ alter table LogStatements
 add foreign key (CustomStatementId) references CustomStatement(CustomStatementId);
 
 status = updated 
+
+12/20/2023
+
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Dec 21, 2023 at 02:16 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `crm54`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ContractTerm`
+--
+
+CREATE TABLE `ContractTerm` (
+  `ContractTermId` int(11) NOT NULL,
+  `ContractId` int(11) DEFAULT NULL,
+  `TermStartDate` date DEFAULT NULL,
+  `TermEndDate` date DEFAULT NULL,
+  `BaseAmt` decimal(10,0) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `ContractTerm`
+--
+
+INSERT INTO `ContractTerm` (`ContractTermId`, `ContractId`, `TermStartDate`, `TermEndDate`, `BaseAmt`) VALUES
+(1, 8, '2023-01-01', '2023-12-31', '100'),
+(2, 8, '2024-01-01', '2024-12-31', '125');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `ContractTerm`
+--
+ALTER TABLE `ContractTerm`
+  ADD PRIMARY KEY (`ContractTermId`),
+  ADD KEY `ContractId` (`ContractId`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `ContractTerm`
+--
+ALTER TABLE `ContractTerm`
+  MODIFY `ContractTermId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `ContractTerm`
+--
+ALTER TABLE `ContractTerm`
+  ADD CONSTRAINT `contractterm_ibfk_1` FOREIGN KEY (`ContractId`) REFERENCES `Contract` (`ContractId`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+status = 'not updated'
